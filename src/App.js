c086f5e04b8b82aafbee17088b3ebb679cf61f6e
Austin Kulp <austin@jerry-78.local>
2022-03-16 18:02:49 -0600

    cheerleader, broh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import logo from './logo.svg';
+import cheerleader from './asset/briebug-cheerleader.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={cheerleader} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Thing
+          
         </a>
       </header>
     </div>
